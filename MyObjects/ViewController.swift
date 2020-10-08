4dbe946d10e7493ce8d9cdcec6845c9611d96926
Rafael Ferreira Rodrigues <rafael.frodrigues@gmail.com>
2020-10-07 23:14:03 -0300

    Initial Commit

---
+++ MyObjects/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  MyObjects
+//
+//  Created by Rafael Ferreira Rodrigues on 07/10/20.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create the moon
+        let sphere = SCNSphere(radius: 0.2)
+        
+        //Set texture
+        let material = SCNMaterial()
+        
+        sphere.materials = [material]
+        
+        material.diffuse.contents = UIImage(named: "art.scnassets/2k_moon_texture.jpg")
+        
+        // Set the the moon as the root node
+        
+        let node = SCNNode(geometry: sphere)
+        
+        node.position = SCNVector3(0, 0.1, -0.5)
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        // Set the scene to the view
+        //sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
